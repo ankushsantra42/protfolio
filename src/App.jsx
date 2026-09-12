ac1896f5030a7668a1c85193316d3a9925c9246f
ankushsantra42 <ankushsantra42@gmail.com>
2026-09-12 22:02:24 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Moon from "./assets/moon.svg?react"
 import Sun from "./assets/sun.svg?react"
-import { RiArrowDownLongLine, RiArrowRightLine, RiArrowRightLongFill, RiArrowRightLongLine, RiChat4Line, RiMailLine, RiMenuLine, RiUserLine,  } from "@remixicon/react"
+import { RiArrowDownLongLine, RiArrowRightLine, RiArrowRightLongFill, RiArrowRightLongLine, RiChat4Line, RiCloseLine, RiMailLine, RiMenuLine, RiUserLine, } from "@remixicon/react"
 import Email from "./assets/email.svg?react";
 import GitHub from "./assets/github.svg?react";
 import Insta from "./assets/instagram.svg?react";
@@ -19,6 +19,7 @@ import Job from "./assets/jobPreparation.png"
 
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
   const [isDarkMode, setIsDarkMode] = useState(() => {
     return localStorage.getItem('portfolio-theme') !== 'light';
   });
@@ -74,64 +75,64 @@ function App() {
   //   console.log(e.target);
   // };
 
-// const handleSubmit = async (e) => {
-//   e.preventDefault();
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
 
-//   const formData = new FormData(e.target);
+  //   const formData = new FormData(e.target);
 
-//   try {
-//     await fetch("/", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/x-www-form-urlencoded",
-//       },
-//       body: new URLSearchParams(formData).toString(),
-//     });
+  //   try {
+  //     await fetch("/", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/x-www-form-urlencoded",
+  //       },
+  //       body: new URLSearchParams(formData).toString(),
+  //     });
 
-//     alert("Message sent successfully!");
+  //     alert("Message sent successfully!");
 
-//     setFormData({
-//       name: "",
-//       email: "",
-//       message: "",
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     alert("Failed to send message.");
-//   }
-// };
+  //     setFormData({
+  //       name: "",
+  //       email: "",
+  //       message: "",
+  //     });
+  //   } catch (error) {
+  //     console.error(error);
+  //     alert("Failed to send message.");
+  //   }
+  // };
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  const form = e.target;
-  const data = new FormData(form);
+    const form = e.target;
+    const data = new FormData(form);
 
-  try {
-    const response = await fetch("/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams(data).toString(),
-    });
+    try {
+      const response = await fetch("/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams(data).toString(),
+      });
 
-    if (!response.ok) {
-      throw new Error(`Form submission failed: ${response.status}`);
+      if (!response.ok) {
+        throw new Error(`Form submission failed: ${response.status}`);
+      }
+
+      alert("Message sent successfully!");
+
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } catch (error) {
+      console.error(error);
+      alert("Failed to send message.");
     }
-
-    alert("Message sent successfully!");
-
-    setFormData({
-      name: "",
-      email: "",
-      message: "",
-    });
-  } catch (error) {
-    console.error(error);
-    alert("Failed to send message.");
-  }
-};
+  };
 
 
   const toggleTheme = () => {
@@ -147,13 +148,52 @@ const handleSubmit = async (e) => {
       <main className='w-full bg-[var(--page-bg)] text-[var(--text-primary)] transition-colors duration-300'
         onMouseMove={handleMouseMove}
       >
-        <nav className='w-full sticky top-0 z-50 flex items-center justify-between px-10 py-4 max-w-7xl mx-auto bg-[var(--page-bg)] transition-colors duration-300'>
+        <nav className='w-full sticky top-0 z-50 flex items-center justify-between px-10 py-4 max-w-7xl mx-auto bg-[var(--page-bg)] transition-colors duration-300 '>
           <div className='text-lg font-medium'>Ankush Santra</div>
           <div className='hidden md:block'>
             <ul className='flex gap-10'>
-              <li  className='hover:scale-120 transition duration-200 ease-in'><a name="home" onClick={scrollToPage} href="#home">Home</a></li>
-              <li className='hover:scale-120 transition duration-200 ease-in'><a name="project" onClick={scrollToPage} href="#project">Projects</a></li>
-              <li className='hover:scale-120 transition duration-200 ease-in'><a name="contact" onClick={scrollToPage} href="#contact">Contact</a></li>
+              <li className='hover:scale-120 transition duration-200 ease-in relative
+      py-2
+      after:absolute
+      after:bottom-0
+      after:left-0
+      after:h-[2px]
+      after:w-full
+      after:origin-right
+      after:scale-x-0
+      after:bg-[var(--accent)] *:
+      after:transition-transform
+      after:duration-300
+      hover:after:origin-left
+      hover:after:scale-x-100'><a name="home" onClick={scrollToPage} href="#home">Home</a></li>
+              <li className='hover:scale-120 transition duration-200 ease-in relative
+      py-2
+      after:absolute
+      after:bottom-0
+      after:left-0
+      after:h-[2px]
+      after:w-full
+      after:origin-right
+      after:scale-x-0
+      after:bg-[var(--accent)]
+      after:transition-transform
+      after:duration-300
+      hover:after:origin-left
+      hover:after:scale-x-100'><a name="project" onClick={scrollToPage} href="#project">Projects</a></li>
+              <li className='hover:scale-120 transition duration-200 ease-in relative
+      py-2
+      after:absolute
+      after:bottom-0
+      after:left-0
+      after:h-[2px]
+      after:w-full
+      after:origin-right
+      after:scale-x-0
+      after:bg-[var(--accent)]
+      after:transition-transform
+      after:duration-300
+      hover:after:origin-left
+      hover:after:scale-x-100'><a name="contact" onClick={scrollToPage} href="#contact">Contact</a></li>
             </ul>
           </div>
           <div className='flex items-center'>
@@ -167,9 +207,62 @@ const handleSubmit = async (e) => {
               {isDarkMode ? <Sun /> : <Moon />}
             </button>
           </div>
-          {/* <div className='md:hidden'>
-            <RiMenuLine />
-          </div> */}
+          <div onClick={() => setMenuOpen(!menuOpen)} className='md:hidden'>
+            {<RiMenuLine />}
+          </div>
+          <div  className={`overflow-hidden absolute top-0 left-0 w-full transition-all duration-500 ease-in-out md:hidden ${
+          menuOpen
+            ? "max-h-96 opacity-100"
+            : "max-h-0 opacity-0"
+        }`}>
+              <div className=' w-full p-10 bg-[var(--page-bg)]'>
+                <div onClick={() => setMenuOpen(!menuOpen)} className='md:hidden absolute right-5'>
+                  {<RiCloseLine />}
+                </div>
+                <ul className='flex w-full flex-col gap-10'>
+                  <li onClick={() => setMenuOpen(false)} className='hover:scale-115 transition duration-200 ease-in py-2
+      after:absolute
+      after:bottom-0
+      after:left-0
+      after:h-[2px]
+      after:w-full
+      after:origin-right
+      after:scale-x-0
+      after:bg-[var(--accent)]
+      after:transition-transform
+      after:duration-300
+      hover:after:origin-left
+      hover:after:scale-x-100 w-fit'><a name="home" onClick={scrollToPage} href="#home">Home</a></li>
+                  <li onClick={() => setMenuOpen(false)} className='hover:scale-115 transition duration-200 ease-in py-2
+      after:absolute
+      after:bottom-0
+      after:left-0
+      after:h-[2px]
+      after:w-full
+      after:origin-right
+      after:scale-x-0
+      after:bg-[var(--accent)]
+      after:transition-transform
+      after:duration-300
+      hover:after:origin-left
+      hover:after:scale-x-100 w-fit'><a name="project" onClick={scrollToPage} href="#project">Projects</a></li>
+                  <li onClick={() => setMenuOpen(false)} className='hover:scale-115 transition duration-200 ease-in py-2
+      after:absolute
+      after:bottom-0
+      after:left-0
+      after:h-[2px]
+      after:w-full
+      after:origin-right
+      after:scale-x-0
+      after:bg-[var(--accent)]
+      after:transition-transform
+      after:duration-300
+      hover:after:origin-left
+      hover:after:scale-x-100 w-fit'><a name="contact" onClick={scrollToPage} href="#contact">Contact</a></li>
+                </ul>
+              </div>
+            </div>
+            
         </nav>
 
         <div
@@ -192,7 +285,7 @@ const handleSubmit = async (e) => {
                 <p className='text-xl'>MERN stack Developer</p>
                 <div className='flex items-center mt-10 md:mt-7 hover:scale-110 transition duration-200 ease-in group'>
                   <a href='/Ankush_cv.pdf' download="Ankush-Santra-Resume.pdf"
-                   className='bg-linear-to-l from-cyan-800 to-blue-900 text-white px-5  py-2 text-base rounded-md flex whitespace-nowrap '>Resume</a>
+                    className='bg-linear-to-l from-cyan-800 to-blue-900 text-white px-5  py-2 text-base rounded-md flex whitespace-nowrap '>Resume</a>
                   <span className='-ml-4 group-hover:-ml-3 transition  duration-200 ease-in '><RiArrowRightLongLine size={35} /></span>
                 </div>
 
@@ -448,15 +541,15 @@ const handleSubmit = async (e) => {
 
               <form
                 name="contact"
-  method="POST"
-  data-netlify="true"
-  onSubmit={handleSubmit}
-  className="flex flex-col gap-5 w-full"
+                method="POST"
+                data-netlify="true"
+                onSubmit={handleSubmit}
+                className="flex flex-col gap-5 w-full"
               >
                 <input type="hidden" name="form-name" value="contact" />
                 {/* Name */}
                 <div className="flex flex-col gap-2">
-                  <label className='flex items-center gap-2' htmlFor="name">< RiUserLine  /> Name</label>
+                  <label className='flex items-center gap-2' htmlFor="name">< RiUserLine /> Name</label>
 
                   <input
                     type="text"
